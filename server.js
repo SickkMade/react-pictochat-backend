29080c6433ef47fecb83fdd01d28e69bdad7d951
sickk <jaakmyr@gmail.com>
2024-09-03 11:28:19 -0700

    changed origin for cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors:{
-        origin: 'https://react-pictochat.netlify.app/',
+        origin: 'https://react-pictochat.netlify.app',
         methods:["GET", "POST"]
     },
 })
