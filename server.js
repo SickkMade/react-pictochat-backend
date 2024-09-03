bb66a5459e309a89962bd94a423deb4269149c98
sickk <jaakmyr@gmail.com>
2024-09-03 10:06:27 -0700

    temp backend, just testing before adding some more

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const http = require('http');
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors:{
+        origin: 'https://react-pictochat.netlify.app/',
+        methods:["GET", "POST"]
+    },
+})
+
+io.on('connection', socket => {
+    socket.on('send-message', message => {
+        socket.broadcast.emit('new-message', message)
+        socket.emit('new-message', message)
+    })
+    // socket.on('user-joined', name => {
+    //     socket.broadcast.emit('user-joined', `${name} joined the server`)
+    //     socket.emit('user-joined', `${name} joined the server`)
+    // })
+})
+
+server.listen('8000', () => {
+    console.log('server is running on port 8000')
+})
